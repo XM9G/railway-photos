cbc90a3756a8791cdbfc6eede96e20a7e20ac0d7
Billy <xm9G@outlook.com>
2024-12-30 12:34:23 +1100

    webhook test

--- functions/discordWebhook.js
+++ functions/discordWebhook.js
@@ -12,7 +12,7 @@ exports.handler = async (event) => {
     };
 
     // Send a POST request to the Discord webhook URL
-    await axios.post('DISCORD_WEBHOOK_URL', payload);
+    await axios.post('https://discord.com/api/webhooks/1323101689204707339/VMAMHFU2sZ25Vb2D3rpFFD28JsYidiCsdNgUqF02-LnJ3YPdUn1AUDcA1HmaKjHNXXc-', payload);
 
     return {
       statusCode: 200,
